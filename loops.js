e4321c54d6befb9f015af9b5b6cf1463a793dfca
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-06-29 20:17:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array){
   for (var i=0; i<25; i++){
     console.log()
     if (i===1){
-      array console.log(`I am $ {1} strange loop`)
+      arrayconsole.log(`I am $ {1} strange loop`)
     }
     else{
       array console.log(`I am $ {i} strange loops`)
